533182c5597642c9d591436c812c94dd3c17df6c
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-09-05 22:38:53 -0500

    Update self_architecture.py

--- 02_UL/code/models/self_architecture.py
+++ 02_UL/code/models/self_architecture.py
@@ -123,7 +123,7 @@ class Proper_arch(nn.Module):
             nn.ConvTranspose2d(8, 16, kernel_size=3, stride=2, padding=1, bias=False),
             #nn.BatchNorm2d(24)
             nn.ReLU(True),
-            nn.ConvTranspose2d(16 8, kernel_size=5, stride=3, padding=1, bias=False),
+            nn.ConvTranspose2d(16, 8, kernel_size=5, stride=3, padding=1, bias=False),
             #nn.BatchNorm2d(12)
             nn.ReLU(True),
             nn.ConvTranspose2d(8, 1, kernel_size=2, stride=2, padding=1, bias=False),
