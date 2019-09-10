b7f8080f33942ad1f14511ddad5017ab062b48ed
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-09-10 12:59:26 -0500

    Update Transformer-Torch.py

--- Transformer/Transformer-Torch.py
+++ Transformer/Transformer-Torch.py
@@ -56,7 +56,7 @@ def get_attn_pad_mask(seq_q, seq_k):
     batch_size, len_k = seq_k.size()
     # eq(zero) is PAD token
     pad_attn_mask = seq_k.data.eq(0).unsqueeze(1)  # batch_size x 1 x len_k(=len_q), one is masking
-    aux = pad_attn_mask.expand(batch_size, len_q, len_k).type(torch.DoubleTensor)
+    aux = pad_attn_mask.expand(batch_size, len_q, len_k).type(torch.uint8)
     print('aux type',aux.type())
     print('aux:', aux)
     return pad_attn_mask.expand(batch_size, len_q, len_k)  # batch_size x len_q x len_k
