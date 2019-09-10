0091a11c86f8adff08b978558384c3b491585cc4
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-09-10 12:06:53 -0500

    Update Transformer-Torch.py

--- Transformer/Transformer-Torch.py
+++ Transformer/Transformer-Torch.py
@@ -160,8 +160,9 @@ class Decoder(nn.Module):
         dec_outputs = self.tgt_emb(dec_inputs) + self.pos_emb(torch.LongTensor([[5,1,2,3,4]]))
         dec_self_attn_pad_mask = get_attn_pad_mask(dec_inputs, dec_inputs)
         dec_self_attn_subsequent_mask = get_attn_subsequent_mask(dec_inputs).type(torch.DoubleTensor)
-        print('dec_self_attn_subsequent_mask',dec_self_attn_subsequent_mask)
-        print('dec_self_attn_subsequent_mask type',dec_self_attn_subsequent_mask.type())
+        aux = dec_self_attn_pad_mask + dec_self_attn_subsequent_mask
+        print('aux',aux)
+        print('aux type',aux.type())
         dec_self_attn_mask = torch.gt((dec_self_attn_pad_mask + dec_self_attn_subsequent_mask), 0)
 
         dec_enc_attn_mask = get_attn_pad_mask(dec_inputs, enc_inputs)
