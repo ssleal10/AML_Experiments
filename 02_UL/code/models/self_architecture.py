af7589538d81be05b446bdde18038c2e0675b878
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-09-09 19:49:21 -0500

    Update self_architecture.py

--- 02_UL/code/models/self_architecture.py
+++ 02_UL/code/models/self_architecture.py
@@ -108,7 +108,7 @@ class Proper_arch(nn.Module):
             nn.ReLU(True),
             nn.ConvTranspose2d(16, 1, 3, stride=1, padding=1),  # b, 1, 28, 28
             nn.ReLU(True),
-            nn.Tanh())
+            nn.Sigmoid())
         
     def forward(self, x):
         #Encoder
