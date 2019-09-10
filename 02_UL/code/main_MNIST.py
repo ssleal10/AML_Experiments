b2d951b83626d3568d18745f5c15aa1478a233fa
ssleal10 <47038702+ssleal10@users.noreply.github.com>
2019-09-09 20:07:41 -0500

    Update main_MNIST.py

--- 02_UL/code/main_MNIST.py
+++ 02_UL/code/main_MNIST.py
@@ -77,7 +77,12 @@ if __name__ == '__main__':
     ])
 
     transform_test = transforms.Compose([
+        transforms.RandomResizedCrop(size=28, scale=(0.2,1.)),
+        transforms.ColorJitter(0.4, 0.4, 0.4, 0.4),
+        transforms.RandomGrayscale(p=0.2),
+        #transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
+        #transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
         transforms.Normalize([0.5], [0.5]),
     ])
 
